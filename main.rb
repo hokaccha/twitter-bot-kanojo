535a819c3ca424ca11e9b005bf5be9903ffa67a6
hokaccha <k.hokamura@gmail.com>
2009-12-09 19:02:41 +0900

    最初のリプライだけを対象にする

--- main.rb
+++ main.rb
@@ -13,7 +13,8 @@ get '/cron/return' do
   memcache = AppEngine::Memcache.new
   tweets = @twitter.user_timeline(@conf['kareshi']);
   tweets.each do |tweet|
-    if tweet['text'] =~ /@#{@conf['user']['username']}/ and memcache.get('last_id') != tweet['id']
+    if tweet['text'] =~ /@#{@conf['user']['username']}/
+      break if memcache.get('last_id') == tweet['id']
       messages = YAML.load_file('messages.yaml')['return']
       message = "@#{@conf['kareshi']} #{messages[rand(messages.length)]}"
       @twitter.update(message)
